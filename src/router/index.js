9f71fb4ca2026a7adadca0383036ab8d2f8db86e
Equinox766 <Equinox766@gmail.com>
2024-02-11 18:09:39 -0300

    Se agrega tailwindCss

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/CountView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/CountView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- src/views/CountView.vue
+++ src/views/CountView.vue
@@ -1,11 +1,13 @@
 <script setup>
-
 </script>
 
 <template>
-    <div>
-        <h1>
-            Count        
-        </h1>
+    <header class="bg-slate-900">
+    <div class="mx-auto max-w-6xl py-10">
+      <h1 class="text-4xl font-extrabold text-white uppercase">Administrador del contador</h1>
+    </div>
+  </header>
+  <div class="mx-auto max-w-6xl py-10">
+      <h1 class="text-4xl font-extrabold text-white uppercase">AGREGA NUMERO</h1>
     </div>
 </template>
